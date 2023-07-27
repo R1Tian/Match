5054453e45bb8cac569691c9afb54dbb0432445c
RSAencrypton <15622109524m@gmail.com>
2023-07-27 18:06:41 +1000

    地图系统的基础点位实现
    地图系统的基础点位实现

---
+++ Assets/Scripts/Map/InitMapSetting.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Map {
+    public enum Setting
+    {
+        EasyEnemy,
+        NormalEnemy,
+        EliteEnemy,
+        Support
+    }
+
+    public class InitMapSetting : MonoBehaviour
+    {
+        public static Dictionary<Setting, Action> MapDic;
+
+        private void Awake()
+        {
+            EnrollDic();
+        }
+
+        private void EnrollDic()
+        {
+            MapDic = new Dictionary<Setting, Action>
+        {
+            { Setting.EasyEnemy, RandomEasyEnemy },
+            { Setting.NormalEnemy, RandomNormalEnemy},
+            { Setting.EliteEnemy, RandomEliteEnemy},
+            { Setting.Support, RandomSupport}
+        };
+
+        }
+
+        private void RandomEasyEnemy() {
+            string[] EasyEnemy = new string[] { "暗影魅魔", "幻影幽狼", "血牙狂战士"};
+            int index = UnityEngine.Random.Range(0, EasyEnemy.Length);
+            Debug.Log(EasyEnemy[index]);
+        }
+        private void RandomNormalEnemy() {
+            string[] NormalEnemy = new string[] { "剧毒蜘蛛", "噬魂幽灵", "堕落收割者", "邪恶骨巫" };
+            int index = UnityEngine.Random.Range(0, NormalEnemy.Length);
+            Debug.Log(NormalEnemy[index]);
+        }
+        private void RandomEliteEnemy() {
+            string[] EliteEnemy = new string[] { "腐败瘟疫使者", "暗夜梦魇", "深渊虚灵", "五彩狂怒者" };
+            int index = UnityEngine.Random.Range(0, EliteEnemy.Length);
+            Debug.Log(EliteEnemy[index]);
+        }
+        private void RandomSupport() {
+            string[] Support = new string[] { "商店", "升级卡片", "回血" };
+            int index = UnityEngine.Random.Range(0, Support.Length);
+            Debug.Log(Support[index]);
+        }
+    }
+}
