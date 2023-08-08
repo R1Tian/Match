5a640468505e3384935d86f0cd7248bb0c2d999d
RSAencrypton <15622109524m@gmail.com>
2023-08-08 14:01:14 +1000

    调整了卡牌仓库的逻辑
    调整了卡牌仓库的逻辑

--- Assets/Scripts/Manager/CardManager.cs
+++ Assets/Scripts/Manager/CardManager.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class CardManager
 {
-    private static Dictionary<int, CardObject> CardDic;
+    private static Dictionary<int, CardObject> CardDic = new Dictionary<int, CardObject>();
 
     public static void OnInitCardDatabase()
     {
         List<CardObject> CardList = new List<CardObject>(ResourcesManager.LoadAllCards("Cards"));
+
 
         foreach (var item in CardList) {
             CardDic.TryAdd(item.id, item);
